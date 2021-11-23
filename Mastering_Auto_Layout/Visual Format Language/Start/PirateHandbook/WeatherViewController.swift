1bb55e5364323b83d040f9cd29b5601d6c2853e7
Michael Rodriguez <miarodriguezur@unal.edu.co>
2021-11-23 15:58:32 -0500

    [Video 8]Visual format language files is finished

--- Mastering_Auto_Layout/Visual Format Language/Start/PirateHandbook/WeatherViewController.swift
+++ Mastering_Auto_Layout/Visual Format Language/Start/PirateHandbook/WeatherViewController.swift
@@ -40,20 +40,33 @@ final class WeatherViewController: UIViewController {
     spacerGuides.forEach(view.addLayoutGuide)
     
     NSLayoutConstraint.activate(
-      [imageViews[0].centerYAnchor.constraint(equalTo: view.centerYAnchor)]
-      + imageViews.map {$0.heightAnchor.constraint(equalTo: $0.widthAnchor)}
-    )
-  }
-}
-
-
-
-
-
-
-
-
-
-
-
-
+        NSLayoutConstraint.constraints(
+            withVisualFormat: """
+              |[leftGap]\
+              -[monday(fullWidth@849)]-(bigGap)\
+              -[tuesday(smaller@849)]-(gap)-[wednesday(smaller@849)]\
+              -(gap)-[rightGap(==leftGap)]|
+              """,
+            options: .alignAllBottom,
+            metrics: {
+              let imageWidth = imageViews[0].image!.size.width
+              let gap: CGFloat = 8
+              return [
+                "fullWidth": imageWidth,
+                "smaller": imageWidth * 0.7,
+                "bigGap": gap * 3,
+                "gap": gap
+              ]
+            }(),
+            views: Dictionary(
+              uniqueKeysWithValues: [
+                (["monday", "tuesday", "wednesday"], imageViews),
+                (["leftGap", "rightGap"], spacerGuides)
+              ].flatMap {(keys, values: [Any]) in zip(keys, values)}
+            )
+          )
+          + [imageViews[0].centerYAnchor.constraint(equalTo: view.centerYAnchor)]
+          + imageViews.map {$0.heightAnchor.constraint(equalTo: $0.widthAnchor)}
+        )
+      }
+    }
